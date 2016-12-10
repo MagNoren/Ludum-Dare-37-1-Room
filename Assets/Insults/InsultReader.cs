d3f3183d44d3b4c8163ceb75b0b51297af820491
Cameron Reid <cam12win@gmail.com>
2016-12-10 11:20:54 +0000

    Added settings and main menu, settings not complete

---
+++ Assets/Insults/InsultReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using UnityEngine;
+
+public class InsultReader : MonoBehaviour {
+
+    public List<string> insults;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void getInsults(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        foreach (var line in lines)
+        {
+            insults.Add(line);
+        }
+    }
+}
---
+++ Assets/Insults/Settings.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+
+public class Settings : MonoBehaviour {
+
+    public List<string> settings;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Gets the settings from the file and makes a list of them
+    /// Every other line is the actual setting. Settings start at 0 with the first name, then 1 with the first result
+    /// </summary>
+    public void getSettings()
+    {
+        var lines = File.ReadAllLines("settings.txt");
+        foreach (var line in lines)
+        {
+            settings.Add(line);
+        }
+    }
+
+    /// <summary>
+    /// Saves the settings
+    /// </summary>
+    public void saveSettings()
+    {
+        File.WriteAllLines("settings.txt", settings.ToArray());
+    }
+
+    /// <summary>
+    /// What controlls the settings canvas view. Doesn't do the buttons
+    /// </summary>
+    public void settingsView()
+    {
+
+    }
+
+}
