079806827358d07aaf617a96d23a52603cfb2047
Cameron Reid <cam12win@gmail.com>
2016-12-10 15:15:32 +0000

    Added npcInsults to BattleScript

--- Assets/Insults/BattleScript.cs
+++ Assets/Insults/BattleScript.cs
@@ -1,23 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleScript : MonoBehaviour {
 
     public InsultReader insultReader;
 
+    public Button playerButtonOne;
+    public Button playerButtonTwo;
+    public Button playerButtonThree;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
-    public void pickInsult()
+    public Text npcInsultText;
+
+    public void npcInsult()
+    {
+        System.Random rnd = new System.Random();
+        int insultIndex = rnd.Next(0, insultReader.insults.Count);
+        npcInsultText.text = insultReader.insults[insultIndex];
+        int randomInsultPower = rnd.Next(1, 4);
+        int npcInsultPower = insultIndex * randomInsultPower;
+    }
+
+    public void playerInsults()
     {
 
     }
+
+
 }
