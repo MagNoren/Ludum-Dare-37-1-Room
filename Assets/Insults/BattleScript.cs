da159ad5fe0e3159dd7f9542f6e3fa93572c1541
Cameron Reid <cam12win@gmail.com>
2016-12-10 14:47:22 +0000

    Added insults folder with some insults in it, changed code

---
+++ Assets/Insults/BattleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScript : MonoBehaviour {
+
+    public InsultReader insultReader;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void pickInsult()
+    {
+
+    }
+}
--- Assets/Insults/InsultReader.cs
+++ Assets/Insults/InsultReader.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
+using System;
 
 using UnityEngine;
 
 public class InsultReader : MonoBehaviour {
 
     public List<string> insults;
+    public List<int> insultPowers;
 
 	// Use this for initialization
 	void Start () {
@@ -19,12 +22,41 @@ public class InsultReader : MonoBehaviour {
 		
 	}
 
-    void getInsults(string filePath)
+
+    public GameManager gameManager;
+    public void getInsults()
     {
-        var lines = File.ReadAllLines(filePath);
-        foreach (var line in lines)
+        if (gameManager.swearsAllowed == true)
         {
-            insults.Add(line);
+            var lines = File.ReadAllLines("Assets\\Insults\\vulgarInsults.txt");
+            foreach (var line in lines)
+            {
+                insults.Add(line);
+            }
+            foreach (var insult in insults)
+            {
+                int index = insults.IndexOf(insult);
+                if (index % 2 == 0)
+                {
+                    //Is even: Leave it
+                }
+                else
+                {
+                    //Is odd: Remove, add to insultPowers
+                    insultPowers.Add(Int32.Parse(insult));
+                    insults.Remove(insult);
+                }
+            }
         }
+        else
+        {
+            //Double backslash because \ is escape so to use \ we escape the escape
+            var lines = File.ReadAllLines("Assets\\Insults\\cleanInsults.txt");
+            foreach (var line in lines)
+            {
+                insults.Add(line);
+            }
+        }
+        
     }
 }
