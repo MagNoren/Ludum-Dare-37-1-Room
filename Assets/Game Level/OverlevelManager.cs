2a35186fe7ba840a584af962f904e13e0e4b5886
Cameron Reid <cam12win@gmail.com>
2016-12-10 20:00:49 +0000

    Added scenes for overlevel and battlescene. Also made some code changes. Not finished this yet
    Nothing will work until these are done

---
+++ Assets/Game Level/OverlevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlevelManager : MonoBehaviour {
+
+    public GameObject Char1;
+    public GameObject Char2;
+    public GameObject Char3;
+    public GameObject Char4;
+    public GameObject Char5;
+    public GameObject Char6;
+    public GameObject Char7;
+    public GameObject Char8;
+
+    public List<int> wonAgainstChars;
+    public List<int> lostAgainstChars;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void wonLostMarks()
+    {
+        if (wonAgainstChars.Contains(1))
+        {
+
+        }
+    }
+
+
+}
